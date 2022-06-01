7be7ef5e7206edb01c9d39d31c474487cbca33b7
Herdina <gherdina7@gmail.com>
2022-06-01 14:47:20 -0300

    Solar System Mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <a href="https://storyset.com/space" target='_blank'><img src={Cat} className='title-img' /></a>
+        <a href="https://storyset.com/space" target='_blank' rel="noreferrer"><img src={Cat} alt="Space Cat" className='title-img' /></a>
         <span className='text-focus-in'>solar<strong className='strong'>system</strong></span>
       </header>
     );
